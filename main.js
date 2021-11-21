3587c46fd46d15100bc2b498ebd62205d2136793
ParthGupta25 <79052804+ParthGupta25@users.noreply.github.com>
2021-11-21 18:59:21 +0530

    SPORTS PAGE

---
+++ main.js
@@ -0,0 +1,16 @@
+function add(){
+var result=Number(document.getElementById("firstinput").value)+
+Number(document.getElementById("secondinput").value);
+window.alert(result)
+};
+function Subtract(){
+var ans=Number(document.getElementById("firstinput").value)-
+Number(document.getElementById("secondinput").value);
+window.alert(ans)
+};
+
+function Divide(){
+var quotient=Number(document.getElementById("firstinput").value)/
+Number(document.getElementById("secondinput").value);
+window.alert(quotient)
+};
